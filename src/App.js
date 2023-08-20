18043222e7cd223b853695b30a8d4bad9562732f
wajiha kouser <wajihawajid5610@gmail.com>
2023-08-20 16:23:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      helloword
+      helloword 
     </div>
   );
 }
